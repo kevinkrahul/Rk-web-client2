d4081a5dc983f0063166ce59fcaf6874c2ec16d7
kevin <kevinrahul.22ec@kct.ac.in>
2025-07-01 12:58:29 +0530

    dummmy bg-category test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,9 +6,9 @@ export default function Home() {
   return(
     <div>
       dsa
-      <section className=" items-center w-full max-md:pb-[10vw]">
+      <section className="my-[40vw] items-center w-full max-md:pb-[10vw]">
       <div>
-        <h1 className="mt-[40vw] text-center font-medium  pt-2 md:p-3 max-sm:mt-6" style={{ fontSize: "clamp(35px, 3vw, 45px)" }}>
+        <h1 className="text-center font-medium  pt-2 md:p-3 max-sm:mt-6" style={{ fontSize: "clamp(35px, 3vw, 45px)" }}>
         Our Product Options
         </h1>
       </div>
@@ -16,6 +16,7 @@ export default function Home() {
           <FeaturesSectionDemo />
         </div>
       </section>
+      dfsdfs
     </div>
   )
 }
